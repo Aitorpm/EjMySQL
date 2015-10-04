2a24f132d9333273b5e60df0132a42ec512ecdda
Aitor Piñeyro <itor95@gmail.com>
2015-10-04 21:16:50 +0200

    Ejercicio mysql

---
+++ src/de/vogella/mysql/first/test/SQL.java
@@ -0,0 +1,23 @@
+package de.vogella.mysql.first.test;
+
+/**
+ * Created by Aitor on 4/10/15.
+ */
+public class SQL {
+}
+
+/*
+SELECT Name FROM Products;
+SELECT Name, Price FROM Products;
+SELECT Name FROM Products WHERE Price <= 200;
+
+SELECT * FROM Products  WHERE Price >= 60 AND Price <= 120;
+SELECT * FROM Products WHERE Price BETWEEN 60 AND 120;
+
+SELECT Name, Price * 100 FROM Products;
+SELECT Name, Price * 100 AS PriceCents FROM Products;
+
+SELECT AVG(Price) FROM Products;
+
+SELECT AVG(Price) FROM Products;
+ */
